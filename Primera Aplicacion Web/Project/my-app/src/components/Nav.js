c58e592ba59ccd7c2c90784814a8c65d13a0b598
Engas027 <daniel.oriol.niso@gmail.com>
2023-04-18 13:19:25 +0200

    Update Nav.js

--- Primera Aplicacion Web/Project/my-app/src/components/Nav.js
+++ Primera Aplicacion Web/Project/my-app/src/components/Nav.js
@@ -1,21 +1,20 @@
 //GH
 //Obtiene la información del dispositivo, muestra el componente Status y tiene botón de cierre de sesión
 import React from "react";
-import { navigate } from "@reach/router";
-
-import { Status } from "./Status";
-import { Footer } from "./Footer";
+import { navigate } from "@reach/router"; 
+import { Status } from "./Status"; //Componente: Status.js
+import { Footer } from "./Footer"; //Componente: Footer.js
 
 export function Nav() {
   function goToLogout() {
-    navigate("/logout");
+    navigate("/logout"); //Función navigate de la librería @reach/router que redirige al user a la pag de cierre de sesión
   }
 
   return (
     <nav className="card">
       <Status />
       <button onClick={goToLogout} className="card-btn">
-        Logout
+        Cerrar sesión
       </button>
       <Footer />
     </nav>
